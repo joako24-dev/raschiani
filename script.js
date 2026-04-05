21bd0fd23e8160d524ba885d05c0d97b451cb89e
joako <moralesjoaquin392@gmail.com>
2026-04-05 20:15:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,688 @@
+// ================== PRODUCTOS ==================
+
+const masvendidos = [
+
+
+
+]
+
+const rasasi = [ 
+{nombre:"Hawas Fire",precio:90000,notas:"", imagen:"rasasi/fire.jpg"},
+
+{nombre:"Hawas Ice",precio:92000,notas:"", imagen:"rasasi/ice.jpg"},
+
+{nombre:"Hawas Malibu",precio:105000,notas:"", imagen:"rasasi/malibu.jpg"},
+
+{nombre:"Hawas Kobra",precio:107000,notas:"", imagen:"rasasi/kobra.jpg"},
+
+{nombre:"Hawas Black",precio:74000,notas:"", imagen:"rasasi/black.jpg"},
+
+{nombre:"Hawas Tropical",precio:105000,notas:"", imagen:"rasasi/tropical.jpg"},
+
+]
+
+const asrar = [ 
+{nombre:"Vanilla Voyage",precio:95000,notas:"", imagen:"asrar/voyage.jpg"},
+
+{nombre:"Coffe Blend ",precio:95000,notas:"", imagen:"asrar/coffe.jpeg"},
+
+{nombre:"Hikaya",precio:95000,notas:"", imagen:"asrar/hikaya.jpeg"},
+
+{nombre:"Rey",precio:95000,notas:"", imagen:"asrar/rey.jpeg"},
+
+]
+
+const bharara = [
+{nombre:"Aqua Dubai",precio:110000,notas:"", imagen:"bharara/aquadubai.jpg"},
+
+{nombre:"Amber Gold",precio:110000,notas:"", imagen:"bharara/gold.jpg"},
+
+{nombre:"Double Bleu",precio:99000,notas:"", imagen:"bharara/bleu.jpg"},
+
+{nombre:"Bharara King",precio:99000,notas:"", imagen:"bharara/bhararaking.jpg"},
+
+{nombre:"Pistacho Kunafa",precio:99000,notas:"", imagen:"bharara/kunafa.jpeg"},
+
+]
+const armaf = [
+{nombre:"Oddysey Homme",precio:72000,notas:".", imagen:"armaf/oddyseyhomme.jpg"},
+
+{nombre:"Oddysey Aqua",precio:79000,notas:".", imagen:"armaf/oddyseyaqua.jpg"},
+
+{nombre:"Oddysey Bhaamas",precio:82000,notas:".", imagen:"armaf/oddyseybhaamas.jpg"},
+
+{nombre:"Oddysey Spectra",precio:82000,notas:".", imagen:"armaf/spectra.jpg"},
+
+{nombre:"Oddysey Mandarin",precio:82000,notas:".", imagen:"armaf/sky.jpeg"},
+
+{nombre:"Club Nuit Intense Man",precio:70000,notas:".", imagen:"armaf/intenseman.jpg"},
+
+{nombre:"Club Nuit Urban Elixir",precio:80000,notas:".", imagen:"armaf/urban.jpg"},
+
+{nombre:"Club Nuit Sillage",precio:82000,notas:".", imagen:"armaf/sillage.jpg"},
+
+{nombre:"Club Nuit Iconic",precio:86000,notas:".", imagen:"armaf/iconic.jpg"},
+
+{nombre:"Club Nuit Maleka",precio:81000,notas:".", imagen:"armaf/maleka.jpg"},
+
+{nombre:"Terra",precio:89000,notas:"", imagen:"armaf/terra.jpg"},
+
+{nombre:"Pacific Aura",precio:80000,notas:"", imagen:"armaf/pacific.jpg"},
+
+{nombre:"Italia",precio:92000,notas:"", imagen:"armaf/italia.jpg"},
+
+{nombre:"Acuatica",precio:92000,notas:"", imagen:"armaf/acuatica.jpeg"},
+
+{nombre:"Beach Party",precio:92000,notas:"", imagen:"armaf/beach.jpeg"},
+
+]
+
+const afnan = [ 
+{nombre:"9 PM Elixir",precio:80000,notas:".", imagen:"afnan/9pmelixir.webp"},
+
+{nombre:"9 PM ",precio:80000,notas:".", imagen:"afnan/9pm.jpeg"},
+
+{nombre:"9 PM Rebel",precio:80000,notas:".", imagen:"afnan/9pmrebel.webp"},
+
+{nombre:"9 AM Dive",precio:78000,notas:"", imagen:"afnan/9am.webp"},
+
+{nombre:"9 PM Night Out",precio:78000,notas:"", imagen:"afnan/out.webp"},
+
+{nombre:"Supremacy Collector",precio:78000,notas:"", imagen:"afnan/supremacy.webp"},
+
+{nombre:"Turathi Electric",precio:84000,notas:"", imagen:"afnan/electric.webp"},
+
+
+]
+
+const Paris = [
+{nombre:"Minya Coco Lush",precio:60000,notas:"", imagen:"paris/minya.jpg"},
+
+{nombre:"Fayora",precio:89000,notas:"", imagen:"paris/fayora.jpg"},
+
+{nombre:"Khair Confection",precio:73000,notas:"", imagen:"paris/khair.jpg"},
+
+{nombre:"Qissa Delicius",precio:55000,notas:"", imagen:"paris/qissa.jpg"},
+
+{nombre:"Khair Pistacho",precio:75000,notas:"", imagen:"paris/pistacho.jpg"},
+
+{nombre:"Moscow Mule",precio:80000,notas:"", imagen:"paris/moscow.jpg"},
+
+{nombre:"Banana Bliss",precio:80000,notas:"", imagen:"paris/banana.jpg"},
+
+{nombre:"Wayfarer",precio:80000,notas:"", imagen:"paris/wayfarer.jpg"},
+
+{nombre:"Dulzura",precio:80000,notas:"", imagen:"paris/dulzura.jpg"},
+
+{nombre:"Khair Felicity",precio:80000,notas:"", imagen:"paris/felicity.jpeg"},
+
+{nombre:"Marshmallow Blush",precio:80000,notas:"", imagen:"paris/blush.jpeg"},
+
+]
+
+const maison = [
+{nombre:"Salvo Elixir",precio:50000,notas:".", imagen:"maison/salvo.jpg"},
+
+{nombre:"Galactic Men",precio:55000,notas:".", imagen:"maison/galacticmen.jpg"},
+
+{nombre:"Luxe",precio:62000,notas:".", imagen:"maison/luxe.jpg"},
+
+{nombre:"Luxe NY",precio:62000,notas:".", imagen:"maison/luxeny.jpg"},
+
+{nombre:"Luxe Street",precio:62000,notas:".", imagen:"maison/luxestreet.jpg"},
+
+{nombre:"Eclipse",precio:69000,notas:".", imagen:"maison/eclipse.jpg"},
+
+{nombre:"La Vivacite Intensa",precio:58000,notas:".", imagen:"maison/vivacite.jpg"},
+
+{nombre:"Intrude",precio:55000,notas:".", imagen:"maison/intrude.jpg"},
+
+{nombre:"Leonie Intense",precio:55000,notas:".", imagen:"maison/leonie.jpg"},
+
+{nombre:"Glacier Bold",precio:55000,notas:".", imagen:"maison/bold.jpeg"},
+
+{nombre:"Toscano Leather",precio:55000,notas:".", imagen:"maison/toscano.jpeg"},
+
+{nombre:"Spectre Malachite",precio:55000,notas:".", imagen:"maison/spectre.jpeg"},
+
+{nombre:"Alora",precio:55000,notas:".", imagen:"maison/alora.jpeg"},
+
+{nombre:"Jean Love Vibe",precio:55000,notas:".", imagen:"maison/vibe.jpeg"},
+
+{nombre:"Your Touch",precio:55000,notas:".", imagen:"maison/your.jpeg"},
+
+{nombre:"Your Touch Intense",precio:55000,notas:".", imagen:"maison/intense.jpeg"},
+]
+
+const frenchavenue = [
+{nombre:"Vulcan Sable ",precio:80000,notas:".", imagen:"french/vulcansable.jpg"},
+
+{nombre:"Vulcan Feu ",precio:95000,notas:"", imagen:"french/vulcanfeu.jpg"},
+
+{nombre:"Vulkan Black",precio:95000,notas:".", imagen:"french/vulcanblack.jpg"},
+
+{nombre:"Liquid Burn",precio:85000,notas:".", imagen:"french/liquidburn.jpg"},
+
+{nombre:"Liquid Limited Edition",precio:85000,notas:".", imagen:"french/limited.jpg"},
+
+{nombre:"Cocoa Morado",precio:85000,notas:".", imagen:"french/cocoa.jpg"},
+
+{nombre:"Spectre Ghost ",precio:80000,notas:".", imagen:"french/spectreghost.jpg"},
+
+{nombre:"Atlantis Extait",precio:96000,notas:"", imagen:"french/atlantisextai.jpg"},
+
+{nombre:"Aether Extrait",precio:95000,notas:"", imagen:"french/aetherextrait.jpg"},
+
+{nombre:"Forbidden Fruit ",precio:110000,notas:"", imagen:"french/forbiddenfruit.jpg"},
+
+{nombre:"Magnetiq",precio:110000,notas:"", imagen:"french/magnetiq.jpg"},
+
+{nombre:"Royal Blend",precio:49000,notas:"", imagen:"french/royal.jpg"},
+
+{nombre:"Royal Blend Negro",precio:49000,notas:"", imagen:"french/negro.jpeg"},
+
+{nombre:"Proud of You",precio:49000,notas:"", imagen:"french/proud.jpg"},
+
+{nombre:"Meringue",precio:80000,notas:"", imagen:"french/meringue.jpg"},
+
+{nombre:"Azzure Oud",precio:75000,notas:"", imagen:"french/azzure.jpg"},
+
+{nombre:"Veneno",precio:103000,notas:"", imagen:"french/veneno.jpg"},
+
+{nombre:"Grecia",precio:103000,notas:"", imagen:"french/grecia.jpg"},
+
+{nombre:"Creme Of Clouds",precio:103000,notas:"", imagen:"french/clouds.jpg"},
+
+{nombre:"Elysia Vanila ",precio:103000,notas:"", imagen:"french/vanila.jpeg"},
+
+{nombre:"Elysia Vanila Sugar",precio:103000,notas:"", imagen:"french/sugar.jpeg"},
+
+{nombre:"Veneno Bianco",precio:103000,notas:"", imagen:"french/bianco.jpeg"},
+
+{nombre:"Safari Breeze",precio:103000,notas:"", imagen:"french/breeze.jpeg"},
+
+{nombre:"Coconut Leche",precio:103000,notas:"", imagen:"french/Leche.jpeg"},
+
+]
+
+const Lattafa = [
+{nombre:"Yara Elixir",precio:60000,notas:"", imagen:"lattafa/yaraelixir.jpg"},
+
+{nombre:"Yara Candy",precio:60000,notas:"", imagen:"lattafa/yaracandy.jpg"},
+
+{nombre:"Yara Tous",precio:62000,notas:"", imagen:"lattafa/yaratous.jpg"},
+
+{nombre:"Eclaire",precio:76000,notas:"", imagen:"lattafa/eclaire.jpg"},
+
+{nombre:"Eclaire Pistacho",precio:74000,notas:"", imagen:"lattafa/eclairep.jpg"},
+
+{nombre:"Asad Elixir",precio:80000,notas:".", imagen:"lattafa/asadelixir.jpg"},
+
+{nombre:"Asad Burbon",precio:75000,notas:".", imagen:"lattafa/asadburnon.jpg"},
+
+{nombre:"Asad Zanibar",precio:75000,notas:".", imagen:"lattafa/asadzansibar.jpg"},
+
+{nombre:"Asad Men",precio:75000,notas:".", imagen:"lattafa/asadmen.jpg"},
+
+{nombre:"Bade Al Oud Amethyst",precio:64000,notas:"", imagen:"lattafa/amethyst.jpg"},
+
+{nombre:"Khamrah Dunkan",precio:66000,notas:"", imagen:"lattafa/dunkan.jpg"},
+
+{nombre:"Khamrah",precio:66000,notas:"", imagen:"lattafa/khamrah.jpeg"},
+
+{nombre:"Khamrah Qahwa",precio:66000,notas:"", imagen:"lattafa/qahwa.jpeg"},
+
+{nombre:"Bade Al Oud Honor",precio:68000,notas:"", imagen:"lattafa/honor.jpg"},
+
+{nombre:"Oud For Glory",precio:64000,notas:"", imagen:"lattafa/glory.jpg"},
+
+{nombre:"Bade Al Oud Sublime",precio:64000,notas:"", imagen:"lattafa/sublime.jpg"},
+
+{nombre:"Noble Blush",precio:64000,notas:"", imagen:"lattafa/blush.jpg"},
+
+{nombre:"Opulent Dubai",precio:70000,notas:"", imagen:"lattafa/opulent.jpg"},
+
+{nombre:"Liam Blue Shine",precio:78000,notas:"", imagen:"lattafa/liam.jpg"},
+
+{nombre:"Nebras Elixir",precio:85000,notas:"", imagen:"lattafa/nebras.jpg"},
+
+{nombre:"Eclaire Banoffi",precio:74000,notas:"", imagen:"lattafa/banoffi.jpg"},
+
+{nombre:"His Confession",precio:78000,notas:"", imagen:"lattafa/his.jpg"},
+
+{nombre:"The Kingdom",precio:60000,notas:"", imagen:"lattafa/kingdom.jpg"},
+
+{nombre:"Mayar Cherry Intense",precio:64000,notas:"", imagen:"lattafa/cherry.jpg"},
+
+{nombre:"Musaman",precio:60000,notas:"", imagen:"lattafa/musaman.jpg"},
+
+{nombre:"Qaed Al Foursan Unlimited",precio:50000,notas:"", imagen:"lattafa/foursan.jpg"},
+
+{nombre:"Atlas",precio:78000,notas:"", imagen:"lattafa/atlas.jpg"},
+
+{nombre:"Emaan",precio:78000,notas:"", imagen:"lattafa/emaan.jpg"},
+
+{nombre:"Angham",precio:78000,notas:"", imagen:"lattafa/angham.jpg"},
+
+{nombre:"Fakhar Black",precio:78000,notas:"", imagen:"lattafa/fakharblack.jpg"},
+
+{nombre:"Fakhar Platin",precio:78000,notas:"", imagen:"lattafa/platin.jpeg"},
+
+{nombre:"Brioche Vainille",precio:78000,notas:"", imagen:"lattafa/brioche.jpg"},
+
+{nombre:"Ameerat Arab",precio:78000,notas:"", imagen:"lattafa/arab.jpg"},
+
+{nombre:"Sakeena",precio:78000,notas:"", imagen:"lattafa/sakeena.jpg"},
+
+{nombre:"Ansaam Gold",precio:78000,notas:"", imagen:"lattafa/ansaam.jpg"},
+
+{nombre:"Now Women",precio:78000,notas:"", imagen:"lattafa/women.jpg"},
+
+{nombre:"Angham Second",precio:78000,notas:"", imagen:"lattafa/second.jpg"},
+
+{nombre:"Dalal Lattafa",precio:78000,notas:"", imagen:"lattafa/dalal.jpg"},
+
+{nombre:"Habik",precio:78000,notas:"", imagen:"lattafa/habik.jpeg"},
+
+{nombre:"Habik Fem",precio:78000,notas:"", imagen:"lattafa/fem.jpeg"},
+
+
+]
+
+// ================== CARRITO (LOCAL STORAGE) ==================
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || {}
+
+// ================== CREAR CATALOGO ==================
+
+function crearCatalogo(lista,contenedor){
+
+const div = document.getElementById(contenedor)
+if(!div) return
+
+div.innerHTML = ""
+
+lista.forEach(item=>{
+div.innerHTML += `
+<div class="card">
+${item.stock === "no" ? '<div class="sin-stock">SIN STOCK</div>' : ''}
+<img src="${item.imagen}" loading="lazy">
+<h3>${item.nombre}</h3>
+<div class="precio-container">
+
+    <div class="precio-principal">
+        $${formatearPrecio(redondearMiles(item.precio * 1.25))}
+        <span class="cuotas">3 cuotas sin interés</span>
+    </div>
+
+    <div class="precio-secundario">
+        $${formatearPrecio(item.precio)} en efectivo / transferencia
+    </div>
+
+</div>
+
+<button class="detalles" onclick="vernotas('${item.nombre}','${item.notas}','${item.imagen}')">
+Ver notas
+</button>
+
+<button class="agregar" 
+${item.stock === "no" ? "disabled" : ""}
+onclick="${item.stock === "no" ? "" : `agregarCarrito('${item.nombre}',${item.precio},'${item.imagen}', this)`}">
+    <span class="texto">Agregar al carrito</span>
+    <span class="carrito-icono-btn">🛒</span>
+</button>
+
+
+</div>
+`
+})
+}
+
+// ================== CARGAR CATALOGOS ==================
+
+crearCatalogo(masvendidos,"catalogo")
+crearCatalogo(Paris,"paris")
+crearCatalogo(frenchavenue,"french")
+crearCatalogo(maison,"maison")
+crearCatalogo(bharara,"bharara")
+crearCatalogo(armaf,"armaf")
+crearCatalogo(afnan,"afnan")
+crearCatalogo(asrar,"asrar")
+crearCatalogo(rasasi,"rasasi")
+crearCatalogo(Lattafa,"lattafa")
+
+// ================== AGREGAR AL CARRITO ==================
+
+function agregarCarrito(nombre,precio,imagen, boton){
+
+  // 🔥 animación carrito
+    if(boton){
+        boton.classList.add("animado")
+
+        setTimeout(()=>{
+        boton.classList.remove("animado")
+        }, 600)
+    }
+
+    if(carrito[nombre]){
+        carrito[nombre].cantidad++
+    }else{
+        carrito[nombre]={
+        precio:precio,
+        cantidad:1,
+        imagen:imagen
+        }
+    }
+
+    actualizarCarrito()
+}
+
+// ================== ACTUALIZAR CARRITO ==================
+
+function actualizarCarrito(){
+
+let lista=document.getElementById("listaCarrito")
+let totalTarjeta = 0
+let totalEfectivo = 0
+let contador=0
+
+// CALCULAR SIEMPRE (aunque no exista el modal)
+for(let item in carrito){
+let datos=carrito[item]
+totalEfectivo += datos.precio * datos.cantidad
+totalTarjeta += redondearMiles(datos.precio * 1.25) * datos.cantidad
+contador+=datos.cantidad
+}
+
+let contadorHTML = document.getElementById("contador")
+if(contadorHTML){
+    contadorHTML.innerText = contador
+
+  // 🔥 animación rebote
+    contadorHTML.classList.add("animado")
+
+    setTimeout(()=>{
+        contadorHTML.classList.remove("animado")
+    }, 400)
+}
+
+// ACTUALIZAR MODAL (SI EXISTE)
+if(lista){
+lista.innerHTML=""
+
+for(let item in carrito){
+let datos=carrito[item]
+
+lista.innerHTML+=`
+<li class="item-carrito">
+<img src="${datos.imagen}" class="img-carrito">
+<div class="info">
+${item} x${datos.cantidad}<br>
+$${formatearPrecio(datos.precio * datos.cantidad)}
+</div>
+</li>
+`
+}
+document.getElementById("totalTarjeta").innerText = formatearPrecio(totalTarjeta)
+document.getElementById("totalEfectivo").innerText = formatearPrecio(totalEfectivo)
+}
+
+// GUARDAR SIEMPRE
+localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+// ================== MODAL CARRITO ==================
+
+function abrirCarrito(){
+document.getElementById("modalCarrito")?.classList.add("activo")
+}
+
+function cerrarCarrito(){
+document.getElementById("modalCarrito")?.classList.remove("activo")
+}
+
+// ================== MODAL NOTAS ==================
+
+function vernotas(nombre,texto,imagen){
+
+document.getElementById("modalTitulo").innerText=nombre
+document.getElementById("modalTexto").innerText=texto
+document.getElementById("modalImagen").src=imagen
+
+document.getElementById("modalnotas").classList.add("activo")
+}
+
+function cerrarnotas(){
+document.getElementById("modalnotas").classList.remove("activo")
+}
+
+// ================== MENU ==================
+
+function toggleMenu(){
+const navbar = document.querySelector(".navbar")
+navbar.classList.toggle("activo")
+}
+
+// ================== VACIAR ==================
+
+function vaciarCarrito(){
+carrito = {}
+actualizarCarrito()
+}
+
+// ================== ENVIAR WHATSAPP ==================
+
+function enviarPedido(){
+
+let formaPago = document.getElementById("formaPago").value
+
+let mensaje="Hola quiero pedir:\n"
+let total = 0
+
+for(let item in carrito){
+let datos=carrito[item]
+
+let precio = datos.precio
+
+if(formaPago === "tarjeta"){
+precio = redondearMiles(datos.precio * 1.25)
+}
+
+
+mensaje+=`${item} x${datos.cantidad} - $${formatearPrecio(precio * datos.cantidad)}\n`
+total+=precio * datos.cantidad
+}
+
+mensaje+=`\nTotal: $${total}`
+
+if(formaPago === "tarjeta"){
+mensaje+=`\nForma de pago: Tarjeta (3 cuotas sin interés)`
+}else{
+mensaje+=`\nForma de pago: Efectivo / Transferencia (25% OFF)`
+}
+
+// copiar
+navigator.clipboard.writeText(mensaje)
+
+// abrir instagram
+window.open(`https://ig.me/m/raschiani_perfumeria`)
+
+alert("Pedido copiado. Pegalo en Instagram 💬")
+}
+// ================== CERRAR MODAL AL HACER CLICK AFUERA ==================
+
+window.addEventListener("click", function(e){
+
+    const modalCarrito = document.getElementById("modalCarrito")
+    const modalNotas = document.getElementById("modalnotas")
+
+    // cerrar carrito
+    if(e.target === modalCarrito){
+        cerrarCarrito()
+    }
+
+    // 🔥 cerrar notas
+    if(e.target === modalNotas){
+        cerrarnotas()
+    }
+
+})
+document.addEventListener("keydown", function(e){
+    if(e.key === "Escape"){
+        cerrarCarrito()
+        cerrarnotas()
+    }
+})
+
+const elementos = document.querySelectorAll(".card")
+
+const mostrarElementos = () => {
+  const trigger = window.innerHeight * 0.85
+
+    elementos.forEach(el => {
+        const top = el.getBoundingClientRect().top
+
+        if(top < trigger){
+        el.classList.add("visible")
+        }
+    })
+}
+
+window.addEventListener("scroll", mostrarElementos)
+window.addEventListener("load", mostrarElementos)
+
+function formatearPrecio(numero){
+return numero.toLocaleString("es-AR")
+}
+
+//CARRUSEL
+const track = document.getElementById("carouselTrack")
+const carousel = document.getElementById("carousel")
+
+if(track){
+
+let index = 0
+let autoPlay
+let startX = 0
+let isDragging = false
+
+// 🔥 CLONAR ELEMENTOS (infinito real)
+const items = Array.from(track.children)
+items.forEach(item => {
+let clone = item.cloneNode(true)
+track.appendChild(clone)
+})
+
+// 🔥 ancho real
+const getWidth = () => track.children[0].offsetWidth + 20
+
+function mover(){
+index++
+track.style.transition = "transform 0.5s ease"
+track.style.transform = `translateX(-${index * getWidth()}px)`
+
+// reset invisible (infinito)
+if(index >= items.length){
+setTimeout(()=>{
+track.style.transition = "none"
+index = 0
+track.style.transform = `translateX(0px)`
+},500)
+}
+}
+
+// 🔥 autoplay
+function iniciarAutoPlay(){
+autoPlay = setInterval(mover, 3000)
+}
+
+function detenerAutoPlay(){
+clearInterval(autoPlay)
+}
+
+// 👉 PAUSA HOVER
+carousel.addEventListener("mouseenter", detenerAutoPlay)
+carousel.addEventListener("mouseleave", iniciarAutoPlay)
+
+// 👉 SWIPE MOBILE
+carousel.addEventListener("touchstart", e=>{
+startX = e.touches[0].clientX
+isDragging = true
+detenerAutoPlay()
+})
+
+carousel.addEventListener("touchmove", e=>{
+if(!isDragging) return
+let moveX = e.touches[0].clientX
+let diff = startX - moveX
+
+track.style.transform = `translateX(-${index * getWidth() + diff}px)`
+})
+
+carousel.addEventListener("touchend", e=>{
+isDragging = false
+let endX = e.changedTouches[0].clientX
+let diff = startX - endX
+
+if(diff > 50){
+index++
+}else if(diff < -50 && index > 0){
+index--
+}
+
+track.style.transition = "transform 0.4s ease"
+track.style.transform = `translateX(-${index * getWidth()}px)`
+
+iniciarAutoPlay()
+})
+
+// iniciar
+iniciarAutoPlay()
+}
+
+async function cargarPrecios(){
+
+try{
+const res = await fetch("https://opensheet.elk.sh/1b64tYrrXdsgR0wwhpVIR9w4jBbLuHD1PUOXe4LcT6OY/precio")
+const data = await res.json()
+
+const listas = [rasasi, asrar, bharara, armaf, afnan, Paris, maison, frenchavenue, Lattafa]
+
+listas.forEach(lista=>{
+lista.forEach(producto=>{
+
+let encontrado = data.find(p => p.nombre === producto.nombre)
+
+if(encontrado){
+producto.precio = parseInt(encontrado.precio)
+producto.stock = encontrado.stock
+}
+
+})
+})
+
+// recargar catálogo
+crearCatalogo(masvendidos,"catalogo")
+crearCatalogo(Paris,"paris")
+crearCatalogo(frenchavenue,"french")
+crearCatalogo(maison,"maison")
+crearCatalogo(bharara,"bharara")
+crearCatalogo(armaf,"armaf")
+crearCatalogo(afnan,"afnan")
+crearCatalogo(asrar,"asrar")
+crearCatalogo(rasasi,"rasasi")
+crearCatalogo(Lattafa,"lattafa")
+
+}catch(error){
+console.log("Error cargando precios:", error)
+}
+}
+
+function redondearMiles(numero){
+return Math.floor(numero / 100) * 100
+}
+
+// ================== INICIAR ==================
+cargarPrecios()
+setInterval(cargarPrecios, 5000) // cada 10 segundos
+actualizarCarrito()
